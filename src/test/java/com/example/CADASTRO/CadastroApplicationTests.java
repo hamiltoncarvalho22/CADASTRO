4dc54feef61e20c03d459e5170e1b9a5a5ea14e1
hamilton carvalho <hamiltoncarvalho22@gmail.com>
2021-06-12 02:49:35 -0300

    inicio

---
+++ src/test/java/com/example/CADASTRO/CadastroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CADASTRO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
